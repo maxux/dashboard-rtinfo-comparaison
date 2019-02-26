5db1504325502afece39230c0bad49292007fd6c
Maxime Daniel <root@maxux.net>
2019-02-26 13:52:34 +0100

    Update python version to keep only minimal code

--- rtinfo-slave-python/rtinfo-slave.py
+++ rtinfo-slave-python/rtinfo-slave.py
@@ -5,33 +5,20 @@ import json
 
 RTINFO_ENDPOINT = "http://clea.maxux.net:8089/json"
 
-# This class is in another file for reusability
-class DashboardSlave():
-    def __init__(self, name):
-        self.name = name
-        self.redis = redis.Redis()
-        self.payload = {}
-
-    def publish(self):
-        self.redis.publish("dashboard", json.dumps({"id": self.name, "payload": self.payload}))
-
-    def sleep(self, seconds):
-        time.sleep(seconds)
-
-slave = DashboardSlave("rtinfo")
+r = redis.Redis()
 
 while True:
     print("[+] rtinfo: fetching")
 
     try:
         response = requests.get(RTINFO_ENDPOINT)
-        slave.payload = response.json()
+        payload = response.json()
 
-        print("[+] rtinfo: %d hosts found" % len(slave.payload['rtinfo']))
+        print("[+] rtinfo: %d hosts found" % len(payload['rtinfo']))
 
-        slave.publish()
+        r.publish("dashboard", json.dumps({"id": "rtinfo", "payload": payload}))
 
     except Exception as e:
         print(e)
 
-    slave.sleep(1)
+    time.sleep(1)
